50b760d3ac112cc61d97cccc518b7ae6c8407914
walid salmi <salamiwalid1999@gmail.com>
2024-03-13 22:03:21 +0000

    tasks

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,31 +1,35 @@
-export interface Student {
+// Define the Student interface
+interface Student {
   firstName: string;
   lastName: string;
   age: number;
   location: string;
 }
 
-const student1: Student = {
-  firstName: 'walid',
-  lastName: 'salmi',
-  age: 25,
-  location: 'rabat',
+// Create two students
+let student1: Student = {
+  firstName: "walid",
+  lastName: "salmi",
+  age: 20,
+  location: "rabat",
 };
 
-const student2 = {
-  firstName: 'hamza',
-  lastName: 'salmi',
-  age: 21,
-  location: 'goulmima',
+let student2: Student = {
+  firstName: "hamza",
+  lastName: "salmi",
+  age: 22,
+  location: "sale",
 };
-const studentsList: Array<Student> = [student1, student2];
 
+// Create an array to hold the students
+let studentsList: Student[] = [student1, student2];
 
-const table: HTMLTableElement = document.createElement('table');
+// Use Vanilla JavaScript to render a table
+let table: HTMLTableElement = document.createElement("table");
 studentsList.forEach((student) => {
-  const row: HTMLTableRowElement = table.insertRow();
-  const cell1: HTMLTableCellElement = row.insertCell();
-  const cell2: HTMLTableCellElement = row.insertCell();
+  let row: HTMLTableRowElement = table.insertRow();
+  let cell1: HTMLTableCellElement = row.insertCell();
+  let cell2: HTMLTableCellElement = row.insertCell();
   cell1.textContent = student.firstName;
   cell2.textContent = student.location;
 });
